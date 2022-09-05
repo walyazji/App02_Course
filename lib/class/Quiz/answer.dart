724d2547fa1bae558c051ffbd7a86b4ff06ba095
walyazji <waseemyazji3@gmail.com>
2022-09-05 07:58:39 +0300

    updated

---
+++ lib/class/Quiz/answer.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Answers extends StatelessWidget {
+  const Answers({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/class/Quiz/question.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Questions extends StatelessWidget {
+  const Questions({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
